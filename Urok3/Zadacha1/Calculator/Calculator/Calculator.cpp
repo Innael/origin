ab3f20230d134feb5cdb2b2526310447e23221fc
Sergey Bolshakov <Illidan.Ice@gmail.com>
2023-10-23 23:09:39 +0300

    TretiyUrok

---
+++ Urok3/Zadacha1/Calculator/Calculator/Calculator.cpp
@@ -0,0 +1,100 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+class Calculator {
+public:
+    double num1;
+    double num2;
+
+    Calculator (double num1, double num2) {
+        this->num1 = num1;
+        this->num1 = num2;
+    }
+
+    bool set_num1(double num1) {
+        this->num1 = num1;
+        if (num1 != 0)
+            return true;       
+    }
+
+    bool set_num2(double num2) {
+        this->num2 = num2;
+        if (num2 != 0)
+            return true;
+    }
+
+    void add (double a, double b) {
+        a = num1;
+        b = num2;
+        std::cout << "num1 + num2 = " << a + b << "\n";
+    }
+    void subtract_1_2(double a, double b) {
+        a = num1;
+        b = num2;
+        std::cout << "num1 - num2 = " << a - b << "\n";
+    }
+
+    void subtract_2_1(double a, double b) {
+        a = num1;
+        b = num2;
+        std::cout << "num2 - num1 = " << b - a << "\n";
+    }
+
+    void multiply(double a, double b) {
+        a = num1;
+        b = num2;
+        std::cout << "num1 * num2 = " << a * b << "\n";
+    }
+
+    void divide_1_2(double a, double b) {
+        a = num1;
+        b = num2;
+        std::cout << "num1 / num2 = " << a / b << "\n";
+    }
+
+    void divide_2_1(double a, double b) {
+        a = num1;
+        b = num2;
+        std::cout << "num2 / num1 = " << b / a << "\n";
+    }
+};
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double resultat = 0;
+    Calculator vychisleniya(0, 0);
+
+    for (int i;;) {
+        for (int j = 0; j < 1; ){
+            std::cout << "Введите num1: ";
+            std::cin >> vychisleniya.num1;
+            j = 1;
+            if (vychisleniya.set_num1(vychisleniya.num1) != 1) {
+                std::cout << "Неверный ввод!" << "\n";
+                j = 0;
+            }
+        }
+
+        for (int j = 0; j < 1; ) {
+            std::cout << "Введите num2: ";
+            std::cin >> vychisleniya.num2;
+            j = 1;
+            if (vychisleniya.set_num2(vychisleniya.num2) != 1) {
+                std::cout << "Неверный ввод!" << "\n";
+                j = 0;
+            }
+        }
+
+        vychisleniya.add(0, 0);
+        vychisleniya.subtract_1_2(0, 0);
+        vychisleniya.subtract_2_1(0, 0);
+        vychisleniya.multiply(0, 0);
+        vychisleniya.divide_1_2(0, 0);
+        vychisleniya.divide_2_1(0, 0);
+    }
+    return 0;
+}
+
---
+++ Urok3/Zadacha2/Schetchik/Schetchik/Schetchik.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+class Counter {
+private:
+    int sch;
+
+    Counter(int sch) {
+        this->sch = sch;        
+    }
+ public:
+    Counter() {
+        sch = 1;
+    }
+
+    void Hochu(std::string vopros) {
+        for (int i = 0; i < 1;) {
+            std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";        
+            std::cin >> vopros;
+            if (vopros == "Да" || vopros == "да") {
+                std::cout << "Введите начальное значение счётчика: ";
+                std::cin >> sch;
+                i = 1;
+            }
+            else if (vopros == "Нет" || vopros == "нет") {
+                i = 1;
+            }
+        }
+    };
+    void Komanda(std::string znak) {
+        for (int i = 0; i < 1; ) {
+            std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+            std::cin >> znak;
+        
+            if (znak == "+") {
+                ++sch;
+            }
+            else if (znak == "-") {
+                --sch;
+            }
+            else if (znak == "=") {
+                std::cout << sch << "\n";
+            }
+            else if (znak == "x" || znak == "х") {
+                std::cout << "До свидания!";
+                i = 1;
+            }
+        }
+    };
+};
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    Counter schet;
+    schet.Hochu("");
+    schet.Komanda("");
+
+    return 0;
+}
+
+
