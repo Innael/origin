6bfad895157cd39c26b00e358b059eb6a87bbeed
Sergey Bolshakov <Illidan.Ice@gmail.com>
2023-11-09 21:17:32 +0300

    Urok5Dorabotka

--- Urok5/Zadacha2/StoronyIUgly/StoronyIUgly/StoronyIUgly.cpp
+++ Urok5/Zadacha2/StoronyIUgly/StoronyIUgly/StoronyIUgly.cpp
@@ -4,44 +4,78 @@
 
 class Figura {
 protected:
+    std::string Imya;
     int storony;
     int dlina_a;
     int dlina_b;
     int dlina_c;
+    int dlina_d;
     int ugol_A;
     int ugol_B;
     int ugol_C;
+    int ugol_D;
 
 public:
-    Figura(int dlina_a, int dlina_b, int dlina_c, int ugol_A, int ugol_B, int ugol_C) {
+    Figura(int dlina_a, int dlina_b, int dlina_c, int dlina_d, int ugol_A, int ugol_B, int ugol_C, int ugol_D) {
         storony = 0;
         this->dlina_a = dlina_a;
         this->dlina_b = dlina_b;
         this->dlina_c = dlina_c;
+        this->dlina_d = dlina_d;
         this->ugol_A = ugol_A;
         this->ugol_B = ugol_B;
         this->ugol_C = ugol_C;
+        this->ugol_D = ugol_D;
     }
 
     Figura() {
+        Imya = "Фигура";
         storony = 0;
         dlina_a = 0;
         dlina_b = 0;
         dlina_c = 0;
+        dlina_d = 0;
         ugol_A = 0;
         ugol_B = 0;
         ugol_C = 0;
+        ugol_D = 0;
     }    
-    virtual void print_info(Figura* ukazatel) {
-        std::cout << "Фигура: \n";
-        std::cout << "Стороны: a = " << dlina_a << " b = " << dlina_b << " c = " << dlina_c << "\n";
-        std::cout << "Углы: A = " << ugol_A << " B = " << ugol_B << " C = " << ugol_C << "\n";
+      std::string print_name() {
+        return Imya;
     }
+      int print_storony() {
+          return storony;
+      }     
+     int print_dlina_a() {
+        return dlina_a;
+    }
+     int print_dlina_b() {
+        return dlina_b;
+    }
+     int print_dlina_c() {
+        return dlina_c;
+    }
+     virtual int print_dlina_d() {
+         return dlina_d;
+     }
+     int print_Ugol_A() {
+        return ugol_A;
+    }
+     int print_Ugol_B() {
+         return ugol_B;
+     }
+     int print_Ugol_C() {
+         return ugol_C;
+     }
+     virtual int print_Ugol_D() {
+         return ugol_D;
+     }
 };
 
 class Triangl : public Figura {
 public:
     Triangl(int dlina_a, int dlina_b, int dlina_c, int ugol_A, int ugol_B, int ugol_C) {
+        Imya = "Треугольник";
         storony = 3;
         this->dlina_a = dlina_a;
         this->dlina_b = dlina_b;
@@ -52,6 +86,7 @@ public:
     }
 
     Triangl() {
+        Imya = "Треугольник";
         storony = 3;
         dlina_a = 10;
         dlina_b = 10;
@@ -60,17 +95,34 @@ public:
         ugol_B = 70;
         ugol_C = 80;
     }
-    void print_info(Figura* ukazatel) override {
-        std::cout << "Треугольник: \n";
-        std::cout << "Стороны: a = " << dlina_a << " b = " << dlina_b << " c = " << dlina_c << "\n";
-        std::cout << "Углы: A = " << ugol_A << " B = " << ugol_B << " C = " << ugol_C << "\n";
+        std::string print_name() {
+            return Imya;
     }
+        int print_dlina_a() {
+            return dlina_a;
+        }
+        int print_dlina_b() {
+            return dlina_b;
+        }
+        int print_dlina_c() {
+            return dlina_c;
+        }
+        int print_Ugol_A() {
+            return ugol_A;
+        }
+        int print_Ugol_B() {
+            return ugol_B;
+        }
+        int print_Ugol_C() {
+            return ugol_C;
+        }
 };
 
 class PryamougolnyiTriangl : public Triangl {
 
 public:
     PryamougolnyiTriangl(int dlina_a, int dlina_b, int dlina_c, int ugol_A, int ugol_B) {
+        Imya = "Прямоугольный треугольник";
         this->dlina_a = dlina_a;
         this->dlina_b = dlina_b;
         this->dlina_c = dlina_c;
@@ -80,6 +132,7 @@ public:
     }
 
     PryamougolnyiTriangl() {
+        Imya = "Прямоугольный треугольник";
         storony = 3;
         dlina_a = 40;
         dlina_b = 50;
@@ -88,10 +141,26 @@ public:
         ugol_B= 60;
         ugol_C = 90;
     }
-    void print_info(Figura* ukazatel) override {
-        std::cout << "Прямоугольный треугольник: \n";
-        std::cout << "Стороны: a = " << dlina_a << " b = " << dlina_b << " c = " << dlina_c << "\n";
-        std::cout << "Углы: A = " << ugol_A << " B = " << ugol_B << " C = " << ugol_C << "\n";
+    std::string print_name() {
+        return Imya;
+    }
+    int print_dlina_a() {
+        return dlina_a;
+    }
+    int print_dlina_b() {
+        return dlina_b;
+    }
+    int print_dlina_c() {
+        return dlina_c;
+    }
+    int print_Ugol_A() {
+        return ugol_A;
+    }
+    int print_Ugol_B() {
+        return ugol_B;
+    }
+    int print_Ugol_C() {
+        return ugol_C;
     }
 };
 
@@ -99,6 +168,7 @@ class RavnobedrennyiTriangl : public Triangl {
 
 public:
     RavnobedrennyiTriangl(int dlina_a, int dlina_b, int ugol_A, int ugol_B) {
+        Imya = "Равнобедренный треугольник";
         this->dlina_a = dlina_a;
         this->dlina_b = dlina_b;
         this->ugol_A = ugol_A;
@@ -108,6 +178,7 @@ public:
     }
  
     RavnobedrennyiTriangl() {
+        Imya = "Равнобедренный треугольник";
         storony = 3;
         dlina_a = 40;
         dlina_b = 40;
@@ -116,16 +187,33 @@ public:
         ugol_B = 50;
         ugol_C = 70;
     }
-    void print_info(Figura* ukazatel) override {
-        std::cout << "Равнобедренный треугольник: \n";
-        std::cout << "Стороны: a = " << dlina_a << " b = " << dlina_b << " c = " << dlina_c << "\n";
-        std::cout << "Углы: A = " << ugol_A << " B = " << ugol_B << " C = " << ugol_C << "\n";
+    std::string print_name() {
+        return Imya;
+    }
+    int print_dlina_a() {
+        return dlina_a;
+    }
+    int print_dlina_b() {
+        return dlina_b;
+    }
+    int print_dlina_c() {
+        return dlina_c;
+    }
+    int print_Ugol_A() {
+        return ugol_A;
+    }
+    int print_Ugol_B() {
+        return ugol_B;
+    }
+    int print_Ugol_C() {
+        return ugol_C;
     }
 };
 
 class RavnostoronnyiTriangl : public Triangl {
 public:
     RavnostoronnyiTriangl(int dlina_a) {
+        Imya = "Равносторонний треугольник";
         this->dlina_a = dlina_a;        
         dlina_b = dlina_a;
         dlina_c = dlina_a;
@@ -135,6 +223,7 @@ public:
     }
 
     RavnostoronnyiTriangl() {
+        Imya = "Равносторонний треугольник";
         storony = 3;
         dlina_a = 50;
         dlina_b = 50;
@@ -143,10 +232,26 @@ public:
         ugol_B = 60;
         ugol_C = 60;
     }
-    void print_info(Figura* ukazatel) override {
-        std::cout << "Равносторонний треугольник: \n";
-        std::cout << "Стороны: a = " << dlina_a << " b = " << dlina_b << " c = " << dlina_c << "\n";
-        std::cout << "Углы: A = " << ugol_A << " B = " << ugol_B << " C = " << ugol_C << "\n";
+    std::string print_name() {
+        return Imya;
+    }
+    int print_dlina_a() {
+        return dlina_a;
+    }
+    int print_dlina_b() {
+        return dlina_b;
+    }
+    int print_dlina_c() {
+        return dlina_c;
+    }
+    int print_Ugol_A() {
+        return ugol_A;
+    }
+    int print_Ugol_B() {
+        return ugol_B;
+    }
+    int print_Ugol_C() {
+        return ugol_C;
     }
 };
 
@@ -156,6 +261,7 @@ protected:
     int ugol_D;
 public:
     Quad(int dlina_a, int dlina_b, int dlina_c, int dlina_d, int ugol_A, int ugol_B, int ugol_C, int ugol_D){
+        Imya = "Четырёхугольник";
         storony = 4;
         this->dlina_a = dlina_a;
         this->dlina_b = dlina_b;
@@ -167,6 +273,7 @@ public:
         this->ugol_D = ugol_D;
     }
     Quad() {
+        Imya = "Четырёхугольник";
         storony = 4;
         dlina_a = 10;
         dlina_b = 10;
@@ -177,16 +284,39 @@ public:
         ugol_C = 90;
         ugol_D = 90;
     }
-    void print_info(Figura* ukazatel) override {
-        std::cout << "Четырёхугольник: \n";
-        std::cout << "Стороны: a = " << dlina_a << " b = " << dlina_b << " c = " << dlina_c << " d = " << dlina_d << "\n";
-        std::cout << "Углы: A = " << ugol_A << " B = " << ugol_B << " C = " << ugol_C << " D = " << ugol_D << "\n";
+    std::string print_name() {
+        return Imya;
+    }
+    int print_dlina_a() {
+        return dlina_a;
+    }
+    int print_dlina_b() {
+        return dlina_b;
+    }
+    int print_dlina_c() {
+        return dlina_c;
+    }
+    int print_dlina_d() override {
+        return dlina_d;
+    }
+    int print_Ugol_A() {
+        return ugol_A;
+    }
+    int print_Ugol_B() {
+        return ugol_B;
+    }
+    int print_Ugol_C() {
+        return ugol_C;
+    }
+    int print_Ugol_D() override {
+        return ugol_D;
     }
 };
 
 class Pryamougolnik : public Quad {
 public:
     Pryamougolnik(int dlina_a, int dlina_b) {
+        Imya = "Прямоугольник";
         this->dlina_a = dlina_a;
         this->dlina_b = dlina_b;
         dlina_c = dlina_a;
@@ -197,6 +327,7 @@ public:
         ugol_D = ugol_A;
     }
     Pryamougolnik() {
+        Imya = "Прямоугольник";
         storony = 4;
         dlina_a = 20;
         dlina_b = 30;
@@ -207,16 +338,39 @@ public:
         ugol_C = 90;
         ugol_D = 90;
     }
-    void print_info(Figura* ukazatel) override {
-        std::cout << "Прямоугольник: \n";
-        std::cout << "Стороны: a = " << dlina_a << " b = " << dlina_b << " c = " << dlina_c << " d = " << dlina_d << "\n";
-        std::cout << "Углы: A = " << ugol_A << " B = " << ugol_B << " C = " << ugol_C << " D = " << ugol_D << "\n";
+    std::string print_name() {
+        return Imya;
+    }
+    int print_dlina_a() {
+        return dlina_a;
+    }
+    int print_dlina_b() {
+        return dlina_b;
+    }
+    int print_dlina_c() {
+        return dlina_c;
+    }
+    int print_dlina_d() override {
+        return dlina_d;
+    }
+    int print_Ugol_A() {
+        return ugol_A;
+    }
+    int print_Ugol_B() {
+        return ugol_B;
+    }
+    int print_Ugol_C() {
+        return ugol_C;
+    }
+    int print_Ugol_D() override {
+        return ugol_D;
     }
 };
 
 class Kvadrat : public Quad {
 public:
     Kvadrat(int dlina_a) {
+        Imya = "Квадрат";
         this->dlina_a = dlina_a;
         dlina_b = dlina_a;
         dlina_c = dlina_a;
@@ -227,6 +381,7 @@ public:
         ugol_D = ugol_A;
     }
     Kvadrat() {
+        Imya = "Квадрат";
         storony = 4;
         dlina_a = 10;
         dlina_b = 10;
@@ -237,16 +392,39 @@ public:
         ugol_C = 90;
         ugol_D = 90;
     }
-    void print_info(Figura* ukazatel) override {
-        std::cout << "Квадрат: \n";
-        std::cout << "Стороны: a = " << dlina_a << " b = " << dlina_b << " c = " << dlina_c << " d = " << dlina_d << "\n";
-        std::cout << "Углы: A = " << ugol_A << " B = " << ugol_B << " C = " << ugol_C << " D = " << ugol_D << "\n";
+    std::string print_name() {
+        return Imya;
+    }
+    int print_dlina_a() {
+        return dlina_a;
+    }
+    int print_dlina_b() {
+        return dlina_b;
+    }
+    int print_dlina_c() {
+        return dlina_c;
+    }
+    int print_dlina_d() override {
+        return dlina_d;
+    }
+    int print_Ugol_A() {
+        return ugol_A;
+    }
+    int print_Ugol_B() {
+        return ugol_B;
+    }
+    int print_Ugol_C() {
+        return ugol_C;
+    }
+    int print_Ugol_D() override {
+        return ugol_D;
     }
 };
 
 class Parralelogram : public Quad {
 public:
     Parralelogram(int dlina_a, int dlina_b, int ugol_A, int ugol_B) {
+        Imya = "Параллелограм";
         storony = 4;
         this->dlina_a = dlina_a;
         this->dlina_b = dlina_b;
@@ -258,6 +436,7 @@ public:
         ugol_D = ugol_B;
     }
     Parralelogram() {
+        Imya = "Параллелограм";
         storony = 4;
         dlina_a = 20;
         dlina_b = 30;
@@ -268,16 +447,39 @@ public:
         ugol_C = 60;
         ugol_D = 120;
     }
-    void print_info(Figura* ukazatel) override {
-        std::cout << "Параллелограм: \n";
-        std::cout << "Стороны: a = " << dlina_a << " b = " << dlina_b << " c = " << dlina_c << " d = " << dlina_d << "\n";
-        std::cout << "Углы: A = " << ugol_A << " B = " << ugol_B << " C = " << ugol_C << " D = " << ugol_D << "\n";
+    std::string print_name() {
+        return Imya;
+    }
+    int print_dlina_a() {
+        return dlina_a;
+    }
+    int print_dlina_b() {
+        return dlina_b;
+    }
+    int print_dlina_c() {
+        return dlina_c;
+    }
+    int print_dlina_d() override {
+        return dlina_d;
+    }
+    int print_Ugol_A() {
+        return ugol_A;
+    }
+    int print_Ugol_B() {
+        return ugol_B;
+    }
+    int print_Ugol_C() {
+        return ugol_C;
+    }
+    int print_Ugol_D() override {
+        return ugol_D;
     }
 };
 
 class Romb : public Quad {
 public:
     Romb(int dlina_a, int ugol_A, int ugol_B) {
+        Imya = "Ромб";
         storony = 4;
         this->dlina_a = dlina_a;
         dlina_b = dlina_a;
@@ -289,6 +491,7 @@ public:
         ugol_D = ugol_B;
     }
     Romb() {
+        Imya = "Ромб";
         storony = 4;
         dlina_a = 30;
         dlina_b = 30;
@@ -299,12 +502,47 @@ public:
         ugol_C = 60;
         ugol_D = 120;    
     }
-    void print_info(Figura* ukazatel) override {
-        std::cout << "Ромб: \n";
-        std::cout << "Стороны: a = " << dlina_a << " b = " << dlina_b << " c = " << dlina_c << " d = " << dlina_d << "\n";
-        std::cout << "Углы: A = " << ugol_A <<  " B = " << ugol_B << " C = " << ugol_C << " D = " << ugol_D << "\n";
+    std::string print_name() {
+        return Imya;
+    }
+    int print_dlina_a() {
+        return dlina_a;
+    }
+    int print_dlina_b() {
+        return dlina_b;
+    }
+    int print_dlina_c() {
+        return dlina_c;
+    }
+    int print_dlina_d() override {
+        return dlina_d;
+    }
+    int print_Ugol_A() {
+        return ugol_A;
+    }
+    int print_Ugol_B() {
+        return ugol_B;
+    }
+    int print_Ugol_C() {
+        return ugol_C;
+    }
+    int print_Ugol_D() override{
+        return ugol_D;
     }
 };
+
+void print_info(Figura* ukazatel) {
+    std::cout << ukazatel->print_name() << ": \n"; 
+    if (ukazatel->print_storony() == 3) {
+        std::cout << "Стороны: a = " << ukazatel->print_dlina_a() << " b = " << ukazatel->print_dlina_b() << " c = " << ukazatel->print_dlina_c() << "\n";
+        std::cout << "Углы: A = " << ukazatel->print_Ugol_A() << " B = " << ukazatel->print_Ugol_B() << " C = " << ukazatel->print_Ugol_C() << "\n";
+    }
+    else if (ukazatel->print_storony() == 4) {
+        std::cout << "Стороны: a = " << ukazatel->print_dlina_a() << " b = " << ukazatel->print_dlina_b() << " c = " << ukazatel->print_dlina_c() << " d = " << ukazatel->print_dlina_d() << "\n";
+        std::cout << "Углы: A = " << ukazatel->print_Ugol_A() << " B = " << ukazatel->print_Ugol_B() << " C = " << ukazatel->print_Ugol_C() << " D = " << ukazatel->print_Ugol_D() << "\n";
+    } 
+}
+
 
 int main()
 {
@@ -319,27 +557,28 @@ int main()
     Pryamougolnik pryamoug(30, 70);
     Kvadrat k(50);
     Parralelogram pr(30, 40, 70, 110);
-    Romb r(30, 75, 115);
+    Romb r(50, 75, 115);
 
-    t.print_info(&t);
+    print_info(&t);
     std::cout << "\n";
-    pt.print_info(&pt);
+    print_info(&pt);
     std::cout << "\n";
-    rt.print_info(&rt);
+    print_info(&rt);
     std::cout << "\n";
-    rvnt.print_info(&rvnt);
+    print_info(&rvnt);
     std::cout << "\n";
-    ch.print_info(&ch);
+    print_info(&ch);
     std::cout << "\n";
-    pryamoug.print_info(&pryamoug);
+    print_info(&pryamoug);
     std::cout << "\n";
-    k.print_info(&k);
+    print_info(&k);
     std::cout << "\n";
-    pr.print_info(&pr);
+    print_info(&pr);
     std::cout << "\n";
-    r.print_info(&r);
+    print_info(&r);
     std::cout << "\n";
 
     return 0;
 }
+
 
