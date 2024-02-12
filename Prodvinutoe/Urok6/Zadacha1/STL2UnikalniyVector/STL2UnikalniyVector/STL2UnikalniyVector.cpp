1b2fc685d0906682b8c4facc877a9c886b54335d
Sergey Bolshakov <Illidan.Ice@gmail.com>
2024-02-13 02:54:09 +0300

    STL2

---
+++ Prodvinutoe/Urok6/Zadacha1/STL2UnikalniyVector/STL2UnikalniyVector/STL2UnikalniyVector.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+void sieve(std::vector<int>& vc) {
+    std::vector<int> vc2{};
+    std::sort(vc.begin(), vc.end(),
+        [](const int& lhs, const int& rhs) {
+            return lhs < rhs;
+        }
+    );
+    int a = vc[0];
+    vc2.push_back(a);
+    for (const auto& elem : vc) {       
+        if (elem != a) {
+            vc2.push_back(elem);
+        }
+        a = elem;
+    }
+    vc = vc2;
+}
+
+void sieve2(std::vector<int>& vc) {
+    std::set<int> set1{};    
+    
+    for (const auto& elem : vc) {
+        set1.insert(elem);
+    }
+   
+    for (const auto& elem : vc) {
+        vc.pop_back();
+    }
+
+    for (const auto& elem : set1) {
+        vc.push_back(elem);
+    }
+
+}
+
+int main()
+{
+    std::vector<int> vec{ 1,2,3,4,3,3,27,7,2,15,43,8,1,8 };
+    std::cout << "[IN]: ";
+    for (const auto& elem : vec) {
+        std::cout << elem << " ";
+    }
+    std::cout << "\n";
+
+    sieve(vec);
+    
+
+    std::cout << "[OUT]: ";
+    for (const auto& elem : vec) {
+        std::cout << elem << " ";
+    }
+
+    std::cout << "\n\n";
+
+    std::vector<int> vec2{ 232,1,4542,577,3,3,577,4542,31,3,2,31,17,8,1,54 };
+        
+    std::cout << "[IN]: ";
+    for (const auto& elem : vec2) {
+        std::cout << elem << " ";
+    }
+    std::cout << "\n";
+
+    sieve2(vec2);
+
+    std::cout << "[OUT]: ";
+    for (const auto& elem : vec2) {
+        std::cout << elem << " ";
+    }
+
+    return 0;
+}
+
---
+++ Prodvinutoe/Urok6/Zadacha2/STL2PechatKonteynera/STL2PechatKonteynera/STL2PechatKonteynera.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <set>
+
+template<class T>
+void print_k(T argument) {
+    for (const auto& elem : argument) {
+        std::cout << elem << " ";
+    }
+}
+
+int main()
+{
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_k(test_set); 
+    std::cout << "\n";
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_k(test_list); 
+    std::cout << "\n";
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_k(test_vector); 
+
+    return 0;
+}
+
+
