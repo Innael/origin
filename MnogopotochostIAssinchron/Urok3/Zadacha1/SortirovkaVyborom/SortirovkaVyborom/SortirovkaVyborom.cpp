32aafbdedf4d170f36c7a9b8a89301c9b3b8b85c
Sergey Bolshakov <Illidan.Ice@gmail.com>
2024-04-15 01:06:47 +0300

    MaAssinUrok3

---
+++ MnogopotochostIAssinchron/Urok3/Zadacha1/SortirovkaVyborom/SortirovkaVyborom/SortirovkaVyborom.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <thread>
+#include <future>
+#include <vector>
+
+
+void s_min(std::vector<int>& v, int n, std::promise<int> prom) {
+    int temp = v[n], nu = n;
+    for (int i = n; i < v.size(); ++i) {
+        if (temp > v[i]) {
+            temp = v[i];
+            nu = i;
+        }
+    }
+    prom.set_value(nu);
+}
+
+
+int main()
+{
+    int t = 0, nu = 0;
+    std::vector<int> v{ 5, 4, 3, 2, 1, 10, 8, 9, 7, 6, 5, 21, 22, 24, 27, 30, 15, 14, 13, 12, 11 };
+
+    for (int i = 0; i < v.size(); ++i) {        
+        std::promise<int> prom;
+        std::future<int> ft_res = prom.get_future();
+        auto ft = std::async(s_min, std::ref(v), i, std::move(prom));
+        nu = ft_res.get();        
+        t = v[i];
+        v[i] = v[nu];
+        v[nu] = t;
+    }
+
+    for (auto& const elem : v) {
+        std::cout << elem << ' ';
+    }
+
+    return 0;
+}
---
+++ MnogopotochostIAssinchron/Urok3/Zadacha2/SvoyFor_each/SvoyFor_each/SvoyFor_each.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <future>
+#include <vector>
+
+
+
+
+void multiply3(int& a) {
+    a *= 3;    
+}
+
+template <class Iterator, class Func>                           
+void parr_for_each(Iterator first, Iterator second, Func f) { 
+    auto as = std::distance(first, second);
+    if (as == 1) {
+        f(*first);      
+    }    
+    else if (as > 2 ) {
+        auto mid = first;
+        std::advance(mid, as / 2);        
+        auto ft1 = std::async(std::launch::deferred, parr_for_each<Iterator, Func>, first, mid, f);       
+        auto ft2 = std::async(std::launch::deferred, parr_for_each<Iterator, Func>, mid, second, f);
+        ft1.get();
+        ft2.get();
+    }
+    else if (as == 2) {
+        auto mid = first;
+        std::advance(mid, 1);
+        f(*first);
+        f(*mid);        
+    }
+}
+
+
+int main()
+{
+
+    std::vector<int> v{ 10, 20, 30, 40, 50, 1, 2, 3, 4, 5, 60, 70, 80, 90, 100 };
+
+    parr_for_each(v.begin(), v.end(), multiply3);
+    for (auto& const elem : v) {
+        std::cout << elem << ' ';
+    }
+
+    return 0;
+}
+
+
