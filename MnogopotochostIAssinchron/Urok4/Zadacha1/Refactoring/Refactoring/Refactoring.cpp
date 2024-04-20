f1a51e8e0a675a9836dc71bbc05811cf2de9f516
Sergey Bolshakov <Illidan.Ice@gmail.com>
2024-04-20 23:01:52 +0300

    MaAssinUrok4Dorabotka2

--- MnogopotochostIAssinchron/Urok4/Zadacha1/Refactoring/Refactoring/Refactoring.cpp
+++ MnogopotochostIAssinchron/Urok4/Zadacha1/Refactoring/Refactoring/Refactoring.cpp
@@ -143,6 +143,37 @@ Line::Line(int _x1, int _y1, int _x2, int _y2) {
 
 }
 
+void Line::shift(int m, int n, int k)
+{	
+		x1 += m; y1 += n;
+		x2 += m; y2 += n;	
+}
+
+void Line::scaleAxis(char axis, int a)
+{		
+		if (axis == 'x') {
+			x1 *= a;
+			x2 *= a;
+		}
+		else if (axis == 'y') {
+			y1 *= a;
+			y2 *= a;
+		}
+		else if (axis == 'z') {
+			z1 *= a;
+			z2 *= a;
+		}		
+}
+
+void Line::scale(int s)
+{	
+		x1 /= s; y1 /= s;
+		x2 /= s; y2 /= s;		
+}
+
+
+
+
 Square::Square(){
 }
 
@@ -156,6 +187,47 @@ Square::Square(int _x1, int _y1, int _x2, int _y2, int _x3, int _y3, int _x4, in
 	type = sqr;
 	square = side_a * side_b;
 }
+
+void Square::shift(int m, int n, int k)
+{	
+		x1 += m; y1 += n;
+		x2 += m; y2 += n;
+		x3 += m; y3 += n;
+		x4 += m; y4 += n;	
+}
+
+void Square::scaleAxis(char axis, int a)
+{	
+		if (axis == 'x') {
+			x1 *= a;
+			x2 *= a;
+			x3 *= a;
+			x4 *= a;
+		}
+		else if (axis == 'y') {
+			y1 *= a;
+			y2 *= a;
+			y3 *= a;
+			y4 *= a;
+		}
+		else if (axis == 'z') {
+			z1 *= a;
+			z2 *= a;
+			z3 *= a;
+			z4 *= a;
+		}		
+}
+
+void Square::scale(int s)
+{	
+		x1 /= s; y1 /= s;
+		x2 /= s; y2 /= s;
+		x3 /= s; y3 /= s;
+		x4 /= s; y4 /= s;	
+}
+
+
+
 
 Cube::Cube(int _x1, int _y1, int _z1, int _x2, int _y2, int _z2, int _x3, int _y3, int _z3, int _x4, int _y4, int _z4, int _x5, int _y5, int _z5, int _x6, int _y6, int _z6, int _x7, int _y7, int _z7, int _x8, int _y8, int _z8)
 {
@@ -176,6 +248,68 @@ Cube::Cube(int _x1, int _y1, int _z1, int _x2, int _y2, int _z2, int _x3, int _y
 	square = 6 * side_a * side_b;
 	volume = side_a * side_b * side_c;
 }
+
+void Cube::shift(int m, int n, int k)
+{
+		x1 += m; y1 += n; z1 += k;
+		x2 += m; y2 += n; z2 += k;
+		x3 += m; y3 += n; z3 += k;
+		x4 += m; y4 += n; z4 += k;
+		x5 += m; y5 += n; z5 += k;
+		x6 += m; y6 += n; z6 += k;
+		x7 += m; y7 += n; z7 += k;
+		x8 += m; y8 += n; z8 += k;		
+}
+
+void Cube::scaleAxis(char axis, int a)
+{	
+		if (axis == 'x') {
+			x1 *= a;
+			x2 *= a;
+			x3 *= a;
+			x4 *= a;
+			x5 *= a;
+			x6 *= a;
+			x7 *= a;
+			x8 *= a;
+		}
+		else if (axis == 'y') {
+			y1 *= a;
+			y2 *= a;
+			y3 *= a;
+			y4 *= a;
+			y5 *= a;
+			y6 *= a;
+			y7 *= a;
+			y8 *= a;
+		}
+		else if (axis == 'z') {
+			z1 *= a;
+			z2 *= a;
+			z3 *= a;
+			z4 *= a;
+			z5 *= a;
+			z6 *= a;
+			z7 *= a;
+			z8 *= a;
+		}	
+}
+
+void Cube::scale(int s)
+{	
+		x1 /= s; y1 /= s; z1 /= s;
+		x2 /= s; y2 /= s; z2 /= s;
+		x3 /= s; y3 /= s; z3 /= s;
+		x4 /= s; y4 /= s; z4 /= s;
+		x5 /= s; y5 /= s; z5 /= s;
+		x6 /= s; y6 /= s; z6 /= s;
+		x7 /= s; y7 /= s; z7 /= s;
+		x8 /= s; y8 /= s; z8 /= s;	
+}
+
+
+
+
 
 Circle::Circle() {
 }
--- MnogopotochostIAssinchron/Urok4/Zadacha1/Refactoring/Refactoring/Shape.h
+++ MnogopotochostIAssinchron/Urok4/Zadacha1/Refactoring/Refactoring/Shape.h
@@ -14,9 +14,9 @@ class Shape
 public:		
 	Shape() = default;					
 	int getType() { return type; }
-	void shift(int m, int n, int k);
-	void scaleAxis(char axis, int a);
-	void scale(int s);
+	virtual void shift(int m, int n, int k);
+	virtual void scaleAxis(char axis, int a);
+	virtual void scale(int s);
 
 	int type = 0;
 	int x1 = 0, y1 = 0, z1 = 0,
@@ -35,28 +35,53 @@ public:
 
 class Line : public Shape {
 public:
+	int x1 = 0, y1 = 0;
 	Line();
 	Line(int _x1, int _y1, int _x2, int _y2);
+	virtual void shift(int m, int n, int k) override;
+	virtual void scaleAxis(char axis, int a) override;
+	virtual void scale(int s) override;
 };
 
 class Square : public Line {
 public:
+	int x3 = 0, y3 = 0,
+		x4 = 0, y4 = 0;
+	    double square = 0;
 	Square();
 	Square(int _x1, int _y1, int _x2, int _y2, int _x3, int _y3, int _x4, int _y4);
+	virtual void shift(int m, int n, int k) override;
+	virtual void scaleAxis(char axis, int a) override;
+	virtual void scale(int s) override;
 };
 
 class Cube : public Square {
 public:
+	int z1 = 0, z2 = 0, z3 = 0, 
+		        z4 = 0,
+		x5 = 0, y5 = 0, z5 = 0,
+		x6 = 0, y6 = 0, z6 = 0,
+		x7 = 0, y7 = 0, z7 = 0,
+		x8 = 0, y8 = 0, z8 = 0;
+	   double volume = 0;
 	Cube(int _x1, int _y1, int _z1, int _x2, int _y2, int _z2, int _x3, int _y3, int _z3, int _x4, int _y4, int _z4, int _x5, int _y5, int _z5, int _x6, int _y6, int _z6, int _x7, int _y7, int _z7, int _x8, int _y8, int _z8);
+	virtual void shift(int m, int n, int k) override;
+	virtual void scaleAxis(char axis, int a) override;
+	virtual void scale(int s) override;
 };
 
 class Circle : public Shape {
 public:
+	int x1 = 0, y1 = 0;
+	double square = 0;
+	double radius = 0;
 	Circle();
 	Circle(int _x1, int _y1, double R);
 };
 
 class Cylinder : public Circle {
 public:
+	double volume = 0;
+	double height = 0;
 	Cylinder(int _x1, int _y1, double R, double H);
 };
